7469fd4a3970f4b703895ad890c755e21ff04ed4
Rutuja Kulkarni <rutujask108@gmail.com>
2026-02-13 00:28:21 +0530

    First test case added

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,7 +2,7 @@ import org.junit.*;
 public class CalculatorTest {
    public void testforPal()
     {
-        Calculator cl = new DemoPalindrome();
+        Calculator cl = new Calculator();
         Assert.assertEquals(12, cl.add(7, 5));
     }
 }
